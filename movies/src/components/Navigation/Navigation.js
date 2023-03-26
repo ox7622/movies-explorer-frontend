8cb1fc5863cd68dc0141332eabb4d63d08ed3e63
Oxana Shovkoplyas <ekmati@yandex.ru>
2023-03-26 23:38:38 +0300

    fix nav links

--- movies/src/components/Navigation/Navigation.js
+++ movies/src/components/Navigation/Navigation.js
@@ -10,10 +10,10 @@ function Navigation({ children, isOpen, handleClick }) {
         <button className="nav__close hover-button" type="button" onClick={handleClick} ></button>
         <div className="nav__container">
             <ul className="nav__links">
-                <li className="nav__list-item"><Link to={'/'} onClick={handleClick} className='nav__link  hover-link hover-opacity'> Главная </Link></li>
+                <li className="nav__list-item"><Link to={'/'} onClick={handleClick} className='link nav__link  hover-link hover-opacity'> Главная </Link></li>
                 {children}
             </ul>
-            <Link to={'/profile'} onClick={handleClick} className="nav__link  nav__link_account hover-button">Аккаунт</Link>
+            <Link to={'/profile'} onClick={handleClick} className="link nav__link  nav__link_account hover-button">Аккаунт</Link>
         </div>
     </nav>
     )
